0bf719059af107154d6cea0df68d5a52f45af248
Aruntg2 <tga3855@gmail.com>
2024-12-26 01:40:43 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,8 +10,8 @@ class Config:
     API_HASH = getenv("API_HASH", "8e039b83a886a2c2b97309ccc6298c20")
     BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:7778283881:AAGXnKgTJOnDQlVM_fsDQuJ6OnJZsR9ki3o")
     FSUB = getenv("FSUB", "TelexOriginals")
-    CHID = int(getenv("CHID", "-982105601"))
-    SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
+    CHID = int(getenv("CHID", "-100164009959"))
+    SUDO = list(map(int, getenv("SUDO", "982105601").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://mongodb+srv://arunready160:arun8899@cluster0.38pp4.mongodb.net/")
     
 cfg = Config()
